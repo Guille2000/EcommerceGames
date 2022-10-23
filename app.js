382732a13546b268767eada55928fa8577405c30
guille <guiacosta0000@gmail.com>
2022-10-22 22:45:23 -0300

    envio email

--- app.js
+++ app.js
@@ -86,29 +86,34 @@ const contenedor = document.querySelector("#contenedor");
 const carritoContenedor = document.querySelector("#carritoContenedor");
 const vaciarCarrito = document.querySelector("#vaciarCarrito");
 const precioTotal = document.querySelector("#precioTotal");
-const activarFuncion = document.querySelector('#activarFuncion')
-const procesarCompra = document.querySelector('#procesarCompra')
+const activarFuncion = document.querySelector("#activarFuncion");
+const procesarCompra = document.querySelector("#procesarCompra");
+const totalProceso = document.querySelector("#totalProceso");
+const formulario = document.querySelector('#procesar-pago')
 
-if(activarFuncion){
-  activarFuncion.addEventListener('click', procesarPedido)
+if (activarFuncion) {
+  activarFuncion.addEventListener("click", procesarPedido);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   carrito = JSON.parse(localStorage.getItem("carrito")) || [];
-  document.querySelector("#activarFuncion").click(procesarPedido)
 
-  mostrarCarrito(); 
-  
+  mostrarCarrito();
+  document.querySelector("#activarFuncion").click(procesarPedido);
 });
+if(formulario){
+  formulario.addEventListener('submit', enviarCompra)
+}
 
-if(vaciarCarrito){
+
+if (vaciarCarrito) {
   vaciarCarrito.addEventListener("click", () => {
     carrito.length = [];
     mostrarCarrito();
   });
 }
 
-if(procesarCompra){
+if (procesarCompra) {
   procesarCompra.addEventListener("click", () => {
     if (carrito.length === 0) {
       Swal.fire({
@@ -125,7 +130,7 @@ if(procesarCompra){
 
 stockProductos.forEach((prod) => {
   const { id, nombre, precio, desc, img, cantidad } = prod;
-  if(contenedor){
+  if (contenedor) {
     contenedor.innerHTML += `
     <div class="card mt-3" style="width: 18rem;">
     <img class="card-img-top" src="${img}" alt="Card image cap">
@@ -150,11 +155,11 @@ const agregarProducto = (id) => {
 
 const mostrarCarrito = () => {
   const modalBody = document.querySelector(".modal .modal-body");
-  if(modalBody){
-  modalBody.innerHTML = "";
-  carrito.forEach((prod) => {
-    const { id, nombre, precio, desc, img, cantidad } = prod;
-    console.log(modalBody)
+  if (modalBody) {
+    modalBody.innerHTML = "";
+    carrito.forEach((prod) => {
+      const { id, nombre, precio, desc, img, cantidad } = prod;
+      console.log(modalBody);
       modalBody.innerHTML += `
       <div class="modal-contenedor">
         <div>
@@ -170,9 +175,9 @@ const mostrarCarrito = () => {
       
   
       `;
-    })
-  };
-  
+    });
+  }
+
   if (carrito.length === 0) {
     console.log("Nada");
     modalBody.innerHTML = `
@@ -183,13 +188,12 @@ const mostrarCarrito = () => {
   }
   carritoContenedor.textContent = carrito.length;
 
-  if(precioTotal){
+  if (precioTotal) {
     precioTotal.innerText = carrito.reduce(
       (acc, prod) => acc + prod.cantidad * prod.precio,
       0
     );
   }
- 
 
   guardarStorage();
 };
@@ -205,30 +209,84 @@ function eliminarProducto(id) {
 }
 function procesarPedido() {
   carrito.forEach((prod) => {
-    console.log(carrito)
-    
-    const contenedorCompra = document.querySelector('#contenedorCompra')
-    console.log(contenedorCompra);
-    const { id, nombre, precio, desc, img, cantidad } = prod;
-    if(contenedorCompra){
-      const div = document.createElement("div");
-      div.innerHTML += `
-            <div class="modal-contenedor">
-              <div>
+    const listaCompra = document.querySelector("#lista-compra tbody");
+    const { id, nombre, precio, img, cantidad } = prod;
+    if (listaCompra) {
+      const row = document.createElement("tr");
+      row.innerHTML += `
+              <td>
               <img class="img-fluid img-carrito" src="${img}"/>
-              </div>
-              <div>
-              <p>Producto: ${nombre}</p>
-            <p>Precio: ${precio}</p>
-            <p>Cantidad :${cantidad}</p>
-            <button class="btn btn-danger"  onclick="eliminarProducto(${id})">Eliminar producto</button>
-              </div>
-            </div>
-            
-        
+              </td>
+              <td>${nombre}</td>
+            <td>${precio}</td>
+            <td>${cantidad}</td>
+            <td>${precio * cantidad}</td>
             `;
-      contenedorCompra.appendChild(div);
-    };
-    })
-  }
+      listaCompra.appendChild(row);
+    }
+  });
+  totalProceso.innerText = carrito.reduce(
+    (acc, prod) => acc + prod.cantidad * prod.precio,
+    0
+  );
+}
 
+ function enviarCompra(e){
+   e.preventDefault()
+   const cliente = document.querySelector('#cliente').value
+   const email = document.querySelector('#correo').value
+
+   if(email === '' || cliente == ''){
+     Swal.fire({
+       title: "¡Debes completar tu email y nombre!",
+       text: "Rellena el formulario",
+       icon: "error",
+       confirmButtonText: "Aceptar",
+   })
+ } else {
+
+  const btn = document.getElementById('button');
+
+document.getElementById('procesar-pago')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_qxwi0jn';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Correo enviado!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
+
+   const spinner = document.querySelector('#spinner')
+   spinner.classList.add('d-flex')
+   spinner.classList.remove('d-none')
+
+   setTimeout(() => {
+     spinner.classList.remove('d-flex')
+     spinner.classList.add('d-none')
+     formulario.reset()
+
+     const alertExito = document.createElement('p')
+     alertExito.classList.add('alert', 'alerta', 'd-block', 'text-center', 'col-12', 'mt-2', 'alert-success')
+     alertExito.textContent = 'Compra realizada correctamente'
+     formulario.appendChild(alertExito)
+
+     setTimeout(() => {
+       alertExito.remove()
+     }, 3000)
+
+
+   }, 3000)
+ }
+ localStorage.clear()
+
+ }
